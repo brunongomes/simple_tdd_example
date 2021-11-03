6e58e1b247e30db2dcc4d424b5de05e63cd5a656
Vinícius Duarte Lopes <vin.duartelopes@gmail.com>
2021-11-03 19:34:15 -0300

    Cria o primeiro teste: capacidade e volume inicial da garrafa

--- src/main/java/com/simple_tdd_example/Garrafa.java
+++ src/main/java/com/simple_tdd_example/Garrafa.java
@@ -2,4 +2,15 @@ package com.simple_tdd_example;
 
 public class Garrafa {
 
+   public Garrafa(int capacidade, int volume) {
+
+   }
+
+   public int getCapacidade() {
+      return 1000;
+   }
+
+   public int getVolume() {
+      return 700;
+   }
 }
